f4880ee1d156f31b46b4f588766ba9e28687ad90
Nitin Kagale <nkagale@happymoney.com>
2020-08-09 22:48:40 -0700

    Adding test file

---
+++ index.test.js
@@ -0,0 +1,7 @@
+describe("Sum numbers", () => {
+    test("it should sum two numbers correctly", () => {
+        const sum = 1 + 2;
+        const expectedResult = 3;
+        expect(sum).toEqual(expectedResult);
+    })
+});
